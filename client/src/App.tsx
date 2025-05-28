8813a06ac8d51051134a9b30ea82ad780a7d2aaf
Yusuf <yusufitopcu@gmail.com>
2025-05-28 16:11:28 +0300

    axios eklendi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,34 @@
-import { toast } from 'react-toastify'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const MyComponent = () => {
-  const handleClick = () => {
-    toast.success('Başarıyla kaydedildi!')
-    // toast.error('Bir hata oluştu!')
-    // toast.info('Bilgilendirme mesajı')
-    // toast.warn('Dikkat edilmesi gereken bir durum')
-  }
+interface User {
+  id: number
+  name: string
+  email: string
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.error(err))
+  }, [])
 
   return (
-    <>
-      <button onClick={handleClick} className="bg-blue-500 text-white px-4 py-2 rounded">
-        Bildirim Göster
-      </button>
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </>
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Kullanıcılar</h1>
+      <ul className="space-y-2">
+        {users.map((user) => (
+          <li key={user.id} className="p-2 border rounded">
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default MyComponent
+export default App
