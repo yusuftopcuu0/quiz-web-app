ae6a7ff7d873d7eaaf80d94421f20c5a6a3d4e65
Yusuf <yusufitopcu@gmail.com>
2025-05-28 13:52:29 +0300

    flowbite eklendi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from 'flowbite-react'
 
 function App() {
   return (
     <>
       <div>
         <h1 className="text-2xl">Hello, World!</h1>
+        <Button>Click me</Button>
       </div>
     </>
   )
