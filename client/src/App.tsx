82eafa0a6d8885c07ffd69c50a58b17bded0fd15
Yusuf <yusufitopcu@gmail.com>
2025-05-28 16:02:05 +0300

    react icons eklendi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
-import Counter from './components/Counter'
+import { GrCloudSoftware } from 'react-icons/gr'
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <>
+      <div>
+        <h1 className="text-2xl mb-4">
+          Let's go for a <GrCloudSoftware className="inline text-yellow-500" />?
+        </h1>
+      </div>
+    </>
   )
 }
 
