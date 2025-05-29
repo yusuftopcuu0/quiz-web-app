47010979f66224498a1801138a2baf475e8821db
Yusuf <yusufitopcu@gmail.com>
2025-05-29 14:09:44 +0300

    HomePage oluşturuldu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,12 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-
-interface User {
-  id: number
-  name: string
-  email: string
-}
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
-  const [users, setUsers] = useState<User[]>([])
-
-  useEffect(() => {
-    axios
-      .get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then((res) => setUsers(res.data))
-      .catch((err) => console.error(err))
-  }, [])
-
   return (
-    <div className="p-4">
-      <h1 className="text-2xl mb-4">Kullanıcılar</h1>
-      <ul className="space-y-2">
-        {users.map((user) => (
-          <li key={user.id} className="p-2 border rounded">
-            {user.name} - {user.email}
-          </li>
-        ))}
-      </ul>
+    <div className="min-h-screen bg-gray-50">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   )
 }
