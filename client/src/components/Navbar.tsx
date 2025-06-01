000dd9e866ee153fdb220c495a6e1f0ef5297ebe
Yusuf <yusufitopcu@gmail.com>
2025-06-01 14:59:17 +0300

    Navbar'daki Dashboard'a onClick eklendi (login sayfasına yönlendiriyor)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -79,7 +79,7 @@ const Navbar = () => {
             </li>
             <li>
               <a
-                href="#"
+                onClick={dashClick}
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500"
               >
                 Dashboard
@@ -87,7 +87,7 @@ const Navbar = () => {
             </li>
             <li>
               <a
-                onClick={dashClick}
+                href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500"
               >
                 İletişim
