dd88ed5d07975af5db1d42681587491afc973b14
Yusuf <yusufitopcu@gmail.com>
2025-06-03 10:29:18 +0300

    Dashboard içeriği değişti

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -7,22 +7,33 @@ function Dashboard() {
         <main className="w-full bg-white border p-4 gap-4 border-b">
           <div>
             <div className="flex flex-col sm:flex-row gap-4">
-              <div className="card bg-gray-300 flex-1 h-20 text-center flex items-center justify-center">
-                1
+              <div className="card bg-white border rounded-xl flex-1 h-20 text-center flex items-center justify-center">
+                Toplam Sınavlar
               </div>
-              <div className="card bg-gray-300 flex-1 h-20 text-center flex items-center justify-center">
-                2
+              <div className="card bg-white border rounded-xl flex-1 h-20 text-center flex items-center justify-center">
+                Öğrenciler
               </div>
-            </div>
-            <div className="card bg-gray-300 mt-5 h-30 text-center flex items-center justify-center">
-              3
-            </div>
-            <div className="card bg-gray-300 mt-5 h-30 text-center flex items-center justify-center">
-              4
+
+              <div className="card bg-white border rounded-xl flex-1 h-20 text-center flex items-center justify-center">
+                Ortalama Puan
+              </div>
+
+              <div className="card bg-white border rounded-xl flex-1 h-20 text-center flex items-center justify-center">
+                Modüller
+              </div>
             </div>
 
-            <div className="card bg-gray-300 mt-5 h-12 text-center flex items-center justify-center">
-              5
+            <div className="flex gap-4 mt-4 flex-col sm:flex-row">
+              <div className="card bg-white border rounded mt-5 h-60 text-center flex items-center justify-center p-2">
+                <h1>Modüle Göre Ortalama Sınav Puanları</h1>
+              </div>
+              <div className="card bg-white border rounded mt-5 h-60 text-center flex items-center justify-center p-2">
+                <h1>Son Etkinlikler</h1>
+              </div>
+
+              <div className="card bg-white border rounded mt-5 h-60 text-center flex items-center justify-center p-2">
+                <h1>Yaklaşan Etkinlikler</h1>
+              </div>
             </div>
           </div>
         </main>
