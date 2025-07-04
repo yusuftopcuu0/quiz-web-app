d3acb3cf3b27d7f86b4387bdc8c2dc67d1673901
Yusuf <yusufitopcu@gmail.com>
2025-07-04 13:44:24 +0300

    Signup page'de şirket istenmeyecek

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -36,17 +36,7 @@ function Signup() {
                 required
               />
             </div>
-            <div>
-              <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900">
-                Şirket
-              </label>
-              <input
-                type="text"
-                id="company"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="(İsteğe Bağlı)"
-              />
-            </div>
+
             <div>
               <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900">
                 Telefon Numarası
