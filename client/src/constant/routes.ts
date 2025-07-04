19964c0d9185837e44cf12ed95f560788746ff03
Yusuf <yusufitopcu@gmail.com>
2025-07-04 18:08:11 +0300

    routes.ts'te temizlik yapıldı

--- client/src/constant/routes.ts
+++ client/src/constant/routes.ts
@@ -5,8 +5,4 @@ export const ROUTES = {
   DASHBOARD: '/dashboard',
   QUIZZES: '/dashboard/quizzes',
   NOT_FOUND: '*',
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> 269b65919aec1ae6587483a8e313c90cc1fbd25d
