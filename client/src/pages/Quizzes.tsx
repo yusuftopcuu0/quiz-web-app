1ebe79706bcb43f7b26f894250800d7bd7838151
Yusuf <yusufitopcu@gmail.com>
2025-07-10 12:07:57 +0300

    feat: display total number of quizzes in the header

--- client/src/pages/Quizzes.tsx
+++ client/src/pages/Quizzes.tsx
@@ -103,7 +103,7 @@ const Quizzes = () => {
   return (
     <>
       <div className="flex flex-col gap-4">
-        <h1 className="text-center">Quizzes</h1>
+        <h1 className="text-center">Quizzes ({quizzes?.length})</h1>
         <hr />
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
